c560e2e8d2062a2a4cff1fd5311b8e25465c54c1
何劭弘 <51521470+hsh778205@users.noreply.github.com>
2021-01-06 13:32:21 +0800

    Update CF915E-Chtholly.cpp

--- CF915E-Chtholly.cpp
+++ CF915E-Chtholly.cpp
@@ -46,29 +46,65 @@ template<class T>inline void write(T x)
 	do{G[++g]=x%10;x/=10;}while(x);
 	for(int i=g;i>=1;--i)putchar('0'+G[i]);putchar('\n');
 }
+int ans;
 struct node
 {
 	int l,r;
-	mutable v;
+	mutable int v;
 	node()
 	{
 		l=r=v=0;
 	}
-	node(int ll,int rr,int)
+	node(int ll,int rr=-1,int vv=1)
 	{
-		
+		l=ll;
+		r=rr;
+		v=vv;
 	}
-	bool operator<(const node z)const
+	bool operator<(const node & z)const
 	{
 		return l<z.l;
 	}
-}
+};
 int n,m;
+set<node>Chtholly;
+#define s Chtholly
+#define IT set<node>::iterator
+IT split(int x)
+{
+	IT it=s.lower_bound(node(x));
+	if(it!=s.end()&&it->l==x) return it;
+	it--;
+	node temp=*it;
+	s.erase(it);
+	s.insert(node(temp.l,x-1,temp.v));
+	return s.insert(node(x,temp.r,temp.v)).first;
+}
+void assign(int l,int r,int k)
+{
+	IT y=split(r+1),x=split(l);
+	int delta=0;
+	for(IT pos=x;pos!=y;pos++)
+	{
+		if(pos->v!=k) delta+=pos->r-pos->l+1;
+	}
+	s.erase(x,y);
+	s.insert(node(l,r,k));
+	ans+=(k)?delta:-delta;
+}
 int main()
 {
-	n=read();
+	ans=n=read();
 	m=read();
-	
+	s.insert(node(1,n,1));
+	int l,r,k;
+	while(m--)
+	{
+		l=read();
+		r=read();
+		k=read();
+		assign(l,r,k-1);
+		write(ans);
+	}
 	return 0;
 }
-
